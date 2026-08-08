599464929c1bb475d6a4a7542c0c33e17e1b3339
Niranjana <115653292+Niranjana05-cloud@users.noreply.github.com>
2026-08-08 19:10:21 +0530

    Create outletMap.ts

---
+++ src/lib/outletMap.ts
@@ -0,0 +1,14 @@
+export const OUTLET_ID_TO_STOCK_NAME: Record<string, string> = {
+  adayar: 'Adyar',
+  anna_nagar: 'Anna Nagar',
+  besant_nagar: 'Besant Nagar',
+  bsr_mall: 'BSR Mall',
+  pallavaram: 'Pallavaram',
+  perumbakkam: 'Perumbakkam',
+  porur: 'Porur',
+  ra_puram: 'RA Puram',
+  royapettah: 'Royapettah',
+  tambaram: 'Tambaram',
+  vadapalani: 'Vadapalani',
+  velachery: 'Velachery',
+};
